fd33c820513848478d7ab7aaea8d97fafe362e73
Nick Bohlen <mail@bohlen-design.de>
2016-07-27 15:19:20 +0000

    Updated '/js/bohlen-art.js' via CloudCannon

--- js/bohlen-art.js
+++ js/bohlen-art.js
@@ -37,7 +37,7 @@ $(function() {
     });
     $(".grid-item").click(function() {
       $(this).find(".buy-section").toggle();
-      $(this).find("img").toggleClass("hide");
+      $(this).find("img").toggleClass("zero-opacity");
     })
 });
 
